9e0c22c7f0981474c137492533b70fbfcca64a2b
fneri <f.francesconeri@gmail.com>
2023-12-10 21:56:54 -0800

    WIP add .data$ plot_median_SABGal_EdU_staining

--- R/plot_median_SABGal_EdU_staining.R
+++ R/plot_median_SABGal_EdU_staining.R
@@ -3,15 +3,15 @@ plot_median_SABGal_EdU_staining <- function(data,
                                             additional_variables,
                                             scale_fill_brewer) {
   ggplot(data = data,
-         aes(x = log10(SABGal_median),
-             y = log10(EdU_median),
-             fill = Condition)
+         aes(x = log10(.data$SABGal_median),
+             y = log10(.data$EdU_median),
+             fill = .data$Condition)
   ) +
     geom_point(size = 3,
                shape = 21,
                color = "black") +
-    geom_vline(data = data_thresholds, aes(xintercept = SABGal_threshold_average_log10)) +
-    geom_hline(data = data_thresholds, aes(yintercept = EdU_threshold_average_log10)) +
+    geom_vline(data = data_thresholds, aes(xintercept = .data$SABGal_threshold_average_log10)) +
+    geom_hline(data = data_thresholds, aes(yintercept = .data$EdU_threshold_average_log10)) +
     {if (length(additional_variables) == 1) facet_grid(cols = vars(!!dplyr::sym(additional_variables[1]))) } +
     {if (length(additional_variables) == 2) facet_grid(cols = vars(!!dplyr::sym(additional_variables[1])),
                                                        rows = vars(!!dplyr::sym(additional_variables[2]))) } +
