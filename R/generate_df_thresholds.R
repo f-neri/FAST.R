d138d720b25159fb5c956851a035031561b3b245
fneri <f.francesconeri@gmail.com>
2023-12-10 21:52:48 -0800

    WIP add .data$ generate_df_thresholds

--- R/generate_df_thresholds.R
+++ R/generate_df_thresholds.R
@@ -1,8 +1,8 @@
 generate_df_thresholds <- function(data,
                                    additional_variables) {
   data %>%
-    dplyr::group_by(plate, !!!dplyr::syms(additional_variables)) %>%
-    dplyr::summarise(EdU_threshold_average = mean(EdU_threshold),
-                     SABGal_threshold_average = mean(SABGal_threshold)) %>%
+    dplyr::group_by(.data$plate, !!!dplyr::syms(additional_variables)) %>%
+    dplyr::summarise(EdU_threshold_average = mean(.data$EdU_threshold),
+                     SABGal_threshold_average = mean(.data$SABGal_threshold)) %>%
     dplyr::ungroup()
 }
