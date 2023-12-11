1981fecfbcace94f838ed0e3debfd77e8c9d6adb
fneri <f.francesconeri@gmail.com>
2023-12-11 10:28:17 -0800

    WIP add .data$ tidy_IAoutput

--- R/tidy_IAoutput.R
+++ R/tidy_IAoutput.R
@@ -14,7 +14,7 @@ tidy_IAoutput <- function(data,
   IAoutput <- if ( any(colnames(IAoutput) %in% "Plot Name") ) {
     IAoutput %>% dplyr::rename(well = "Plot Name")
   } else {
-    IAoutput %>% dplyr::rename(well = Name)
+    IAoutput %>% dplyr::rename(well = .data$Name)
   }
   
   # remove extra columns
@@ -30,14 +30,14 @@ tidy_IAoutput <- function(data,
   # pivot_longer()
   OBJ_vec <- colnames(IAoutput)[-(1:2)]
   
-  tidy_data1 <-  tidyr::pivot_longer(IAoutput, all_of(OBJ_vec),
+  tidy_data1 <-  tidyr::pivot_longer(IAoutput, dplyr::all_of(OBJ_vec),
                                      names_to = "cell_ID",
                                      values_to = "Signal_Intensity")
   
   # adjust Channel column name and values
   
   tidy_data1 <- tidy_data1 %>%
-    dplyr::rename(Measured_Parameter = Channel) %>%
+    dplyr::rename(Measured_Parameter = .data$Channel) %>%
     dplyr::mutate(Measured_Parameter = dplyr::case_when(
       Measured_Parameter == 0 ~ "Nuclear_Area",
       Measured_Parameter == 1 ~ "DAPI",
@@ -49,14 +49,14 @@ tidy_IAoutput <- function(data,
   # adjust well column values - extract first letter followed by 2 digits
   
   tidy_data1 <- tidy_data1 %>%
-    dplyr::mutate(well = stringr::str_extract(well, "[A-Za-z]\\d{2}")) %>%
-    dplyr::select(well, cell_ID, dplyr::everything()) # rearrange column order
+    dplyr::mutate(well = stringr::str_extract(.data$well, "[A-Za-z]\\d{2}")) %>%
+    dplyr::select(.data$well, .data$cell_ID, dplyr::everything()) # rearrange column order
   
   # pivot_wider()
   
   tidy_data2 <- tidy_data1 %>%
-    tidyr::pivot_wider(names_from = Measured_Parameter,
-                       values_from = Signal_Intensity) %>%
+    tidyr::pivot_wider(names_from = .data$Measured_Parameter,
+                       values_from = .data$Signal_Intensity) %>%
     stats::na.omit() # remove cells that had with blank/NA signal intensities in IAoutput file
   
   print("completed tidying IA output")
