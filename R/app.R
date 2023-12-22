ac921ace40fba055e64685cc1a4a3576ee7bb519
fneri <f.francesconeri@gmail.com>
2023-12-21 16:26:44 -0800

    fixed Browser argument to allow non-browser app launch

--- R/app.R
+++ R/app.R
@@ -28,68 +28,74 @@ FAST.R <- function(Browser = FALSE) { # app needs to be wrapped in function to b
   on.exit(options(old_options)) # ensures user options are restored upon exit
   options(shiny.maxRequestSize=20*1024^2)
   
-# UI ----------------------------------------------------------------------
-
-ui <- fluidPage(
-  shinyjs::useShinyjs(), # enables shinyjs functions
-  waiter::useWaiter(), # enables waiter functions
-  shinyFeedback::useShinyFeedback(), # enables ShinyFeedback
-  
-  waiter::autoWaiter(color = "white",
-                     html = tagList(br(),
-                                    waiter::spin_hexdots(),
-                                    tagAppendAttributes(style = "color:black",
-                                                        div(
-                                                          br(),br(),
-                                                          p("Loading ...")
-                                                        ))
-                                    )
-                     ),
-  
-  tags$head( # changes CSS style of validate() text
-    tags$style(HTML("
-      .shiny-output-error-validation {
-        color: #ff0000;
-        font-weight: bold;
-      }
-    "))
-  ),
-  
-  titlePanel(
-    h1(strong("FAST-R"), align = "center")
-    ),
-  
-  # Title + paper link
-  fluidRow(
-    column(12,
-           h1("FAST Data Analysis & Visualization App", align = "center"), # TO ADD a(href = "link", "FAST")
-           br()
-           )
-  ),
+  # UI ----------------------------------------------------------------------
   
   ui <- fluidPage(
+    shinyjs::useShinyjs(), # enables shinyjs functions
+    waiter::useWaiter(), # enables waiter functions
+    shinyFeedback::useShinyFeedback(), # enables ShinyFeedback
     
-    tabsetPanel(
-      tabPanel("Data Analysis",
-               data_analysisUI("data_analysis")),
+    waiter::autoWaiter(color = "white",
+                       html = tagList(br(),
+                                      waiter::spin_hexdots(),
+                                      tagAppendAttributes(style = "color:black",
+                                                          div(
+                                                            br(),br(),
+                                                            p("Loading ...")
+                                                          ))
+                                      )
+                       ),
+    
+    tags$head( # changes CSS style of validate() text
+      tags$style(HTML("
+        .shiny-output-error-validation {
+          color: #ff0000;
+          font-weight: bold;
+        }
+      "))
+    ),
+    
+    titlePanel(
+      h1(strong("FAST-R"), align = "center")
+      ),
+    
+    # Title + paper link
+    fluidRow(
+      column(12,
+             h1("FAST Data Analysis & Visualization App", align = "center"), # TO ADD a(href = "link", "FAST")
+             br()
+             )
+    ),
+    
+    ui <- fluidPage(
       
-      tabPanel("Data Visualization",
-               data_visualizationUI("data_visualization"))
+      tabsetPanel(
+        tabPanel("Data Analysis",
+                 data_analysisUI("data_analysis")),
+        
+        tabPanel("Data Visualization",
+                 data_visualizationUI("data_visualization"))
+      )
     )
   )
-)
+      
     
+  # Server ------------------------------------------------------------------
   
-# Server ------------------------------------------------------------------
-
-server <- function(input, output, session) {
-  
-  data_analysisServer("data_analysis")
-  
-  data_visualizationServer("data_visualization")
-  
+  server <- function(input, output, session) {
+    
+    data_analysisServer("data_analysis")
+    
+    data_visualizationServer("data_visualization")
+    
   }
-
-shinyApp(ui, server, options = list(launch.browser = Browser))
-
+  
+  # Launch app --------------------------------------------------------------
+  
+  if (Browser == FALSE) {
+    shinyApp(ui = ui, server = server)
+  } else {
+    shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
+  }
+  
 }
